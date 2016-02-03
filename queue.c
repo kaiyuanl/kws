765e94afa49c01ed4856cda69e925727093e5dd7
Liang Kaiyuan <liangkaiyuan@hotmail.com>
2016-02-03 13:45:38 +0800

    Refactoring functions related to queue

--- queue.c
+++ queue.c
@@ -1,2 +1,99 @@
 #include "proto.h"
 
+int kws_queue_init(struct kws_queue *queue, unsigned int size)
+{
+	spin_lock_init(&(queue->lock));
+	init_waitqueue_head(&(queue->wq));
+
+	queue->size = size;
+	queue->items = (void *)kmalloc(size * sizeof(void *), GFP_KERNEL);
+	if(queue->items == NULL)
+		return -1;
+
+	memset(items, NULL, size * sizeof(void *));
+
+	queue->in = queue->out = 0;
+	queue->count = 0;
+	return 0;
+}
+
+bool kws_queue_empty(struct kws_queue *queue)
+{
+	return (queue->count == 0);
+}
+
+bool kws_queue_full(struct kws_queue *queue)
+{
+	return (queue->count == queue->size);
+}
+
+int kws_queue_in(struct kws_queue *queue, void *item)
+{
+	if (queue == NULL)
+		return -1;
+	if (item == NULL)
+		return -1;
+
+	spin_lock(&(queue->lock));
+	if (queue->count >= queue->size)
+	{
+		spin_unlock(&(queue->lock));
+		return -1;
+	}
+
+	queue->items[(queue->in)%(queue->size)] = item;
+	(queue->in)++;
+	(queue->count)++;
+	spin_unlock(&(queue->lock));
+	return 0;
+}
+
+void *kws_queue_out(struct kws_queue *queue)
+{
+	void *item;
+	if (queue == NULL)
+		return NULL;
+	spin_lock(&(queue->lock));
+	if (queue->count <= 0)
+	{
+		spin_unlock(&(queue->lock));
+		return NULL;
+	}
+
+	item = queue->items[(queue->out)%(queue->size)];
+	(queue->out)++;
+	(queue->count)--;
+	spin_unlock(&(queue->lock));
+	return item;
+}
+
+void kws_queue_release(struct kws_queue *queue)
+{
+	kfree(queue->items);
+}
+
+int kws_request_queue_init(struct kws_queue *queue)
+{
+	return kws_queue_init(queue);
+}
+
+bool kws_request_queue_empty(struct kws_queue *queue)
+{
+	return kws_queue_empty(queue);
+}
+
+bool kws_request_queue_full(struct kws_queue *queue)
+{
+	return kws_queue_full(queue);
+}
+
+int kws_request_queue_in(struct kws_queue *queue,
+	struct kws_request *request)
+{
+	return kws_queue_in(queue, (void *)request);
+}
+
+struct kws_request *kws_request_queue_out(struct kws_queue *queue)
+{
+	return (struct kws_request *)kws_request_queue_out(queue);
+}
